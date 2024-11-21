330223ae7574787df22c54dc4dcd09491243454e
Oleg Alieisieienko <oleg.alieksieienko@taxify.eu>
2024-11-21 08:40:17 +0200

    remove legacy

--- src/legacy/configs/keys.ts
+++
@@ -1,9 +0,0 @@
-export module Keys {
-    export const OpenAIKey = "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";
-}
-
-export module APIKeys {
-    export const InfinitiveKey = "infinitive";
-    export const VariantsKey = "variants";
-    export const SearchVariants = "searchVariants";
-}
