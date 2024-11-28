e741eef8c3b82af27a8780f45eeed58e2c6bd72f
Oleg Alieisieienko <oleg.alieksieienko@taxify.eu>
2024-11-28 07:27:52 +0200

    situationGetPopulars

---
+++ src/Situation/situationGetPopulars.ts
@@ -0,0 +1,17 @@
+import { SituationPreviewModel } from '../Models/SituationPreview';
+
+
+
+Parse.Cloud.define('situationGetPopulars', async (params: any): Promise<any> => {
+
+    let query = new Parse.Query('Situation');
+    query.limit(25);
+    query.descending('updatedAt');
+    const situations = await query.find();
+
+    const items = await Promise.all(
+        situations.map(situation => SituationPreviewModel.fromParse(situation))
+    );
+
+    return { items: items.map(item => item.toJSON()) };
+});
